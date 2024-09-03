dfb188cbc937cc1161524a0eb7fe195169881728
Ahmad Arif <ahmad.arif.pk.98@gmail.com>
2024-09-03 12:29:48 +0500

    Added users and passwords

--- auth_and_permissions/urls.py
+++ auth_and_permissions/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from auth_and_permissions import views
 
+# admin    admin
+# user1    user***123
+# user2    user***456
 urlpatterns = [
     path('', views.index, name='home'),
     path('login', views.login_user, name='login'),
